78e27e1a476bab9ed11c277333191459514ebefd
Littleeich <Littleeich@gmail.com>
2018-01-14 13:51:51 +0200

    Temp file words

--- Parser/Main.java
+++ Parser/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+//package com.company;
 
 import java.text.ParseException;
 import java.text.DateFormat;
---
+++ Parser/words.java
@@ -0,0 +1,6 @@
+public class words {
+    public static void main(String[] args){
+        public String str = "ssgsg sgsgsg sggs sggg";
+        System.out.println(str.split(" ").length);
+    }
+}
