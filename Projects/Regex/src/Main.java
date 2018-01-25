efcb26104d6e2d6dbc67c8d9f08f747a2ad59f53
Littleeich <Littleeich@gmail.com>
2018-01-25 20:56:15 +0200

    small fix of regex

--- Projects/Regex/src/Main.java
+++ Projects/Regex/src/Main.java
@@ -2,12 +2,13 @@
 public class Main {
 
     public static void main(String[] args) {
-	 String first = "joxid sfsef sgsgrd fgfhh";
+	 String first = "65@gmail.com " +
+             "s445ghvh@yandex.ru";
     System.out.println(checkCompanyEmail(first));
     }
 
     public static boolean checkCompanyEmail(String str){
-        String temp = str.replaceAll("(?:\\d?\\w)+(?:@gmail\\.com|@yandex\\.ru),?", "");
+        String temp = str.replaceAll("(?:\\w)+(?:@gmail\\.com|@yandex\\.ru),?", "");
         if(temp.trim().equals(""))
             return true;
         else
