982dbe4ae3f6f7191abb347881b40e38b2d7b12f
Littleeich <Littleeich@gmail.com>
2018-01-28 11:17:08 +0200

    Regex with String Matches

--- Projects/Regex/src/Main.java
+++ Projects/Regex/src/Main.java
@@ -2,16 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
-	 String first = "65@gmail.com " +
-             "s445ghvh@yandex.ru";
+	 String first = "sfs44sf@gmail.com " + "vlad.hil@gmail.com" +
+             "sghvh@yandex.ru";
     System.out.println(checkCompanyEmail(first));
     }
 
     public static boolean checkCompanyEmail(String str){
-        String temp = str.replaceAll("(?:\\w)+(?:@gmail\\.com|@yandex\\.ru),?", "");
-        if(temp.trim().equals(""))
-            return true;
-        else
-            return false;
+        return str.matches("(?:(?:\\w)+[.-]?(?:\\w)+(?:@gmail\\.com|@yandex\\.ru),? ?)+");
     }
 }
