c3ec3642ef2dea4e48805efacf2c5b8f7e4f10f7
Littleeich <Littleeich@gmail.com>
2018-01-31 00:23:24 +0200

    Not less than 1450 and from 0 to 100 regex

--- Projects/Regex/src/Main.java
+++ Projects/Regex/src/Main.java
@@ -10,4 +10,12 @@ public class Main {
     public static boolean checkCompanyEmail(String str){
         return str.matches("(?:(?:\\w)+[.-]?(?:\\w)+(?:@gmail\\.com|@yandex\\.ru),? ?)+");
     }
+
+    public static boolean checkNotLessThan1450(String str){
+        return str.matches("(?:(?:\\d{5,}|145[\\d]|14[6-9][0-9]|1[5-9]\\d{2}|[2-9]\\d{3})(?:.\\d+)?,? ?)+[^.\\w\\d]");
+    }
+
+    public static boolean checkFrom0To100 (String str){
+        return str.matches("((?:100|\\d{1,2})(?:\\.\\d+)?,? ?)+");
+    }
 }
