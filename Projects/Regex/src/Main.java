f36c13743522090e1c020612055e6a6789c36057
Littleeich <Littleeich@gmail.com>
2018-02-01 19:22:19 +0200

    Unit

--- Projects/Regex/src/Main.java
+++ Projects/Regex/src/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args) {
 	 String first = "sfs44sf@gmail.com " + "vlad.hil@gmail.com" +
              "sghvh@yandex.ru";
-    System.out.println(checkCompanyEmail(first));
+	 String second = "1265";
+	 String third = "1800";
+
+	 unitTestForEmailTrue(first);
+	 unitTestForEmailFalse(first);
+	 unitTestFor1450True(second);
+	 unitTestFor1450False(second);
+	 unitTestFor100True(third);
+	 unitTestFor100False(third);
     }
 
     public static boolean checkCompanyEmail(String str){
@@ -12,10 +20,53 @@ public class Main {
     }
 
     public static boolean checkNotLessThan1450(String str){
-        return str.matches("(?:(?:\\d{5,}|[2-9]\\d{3}|1[5-9]\\d{2}|14[6-9][0-9]|145[\\d])(?:.\\d+)?,? ?)+[^.\\w\\d]");
+        return str.matches("(?:(?:\\d{5,}|[2-9]\\d{3}|1[5-9]\\d{2}|14[6-9][0-9]|145[\\d])(?:.\\d+)?,? ?)+[^.\\w\\d]?");
     }
 
     public static boolean checkFrom0To100 (String str){
-        return str.matches("((?:\\d{1,2}|100)(?:\\.\\d+)?,? ?)+");
+        return str.matches("^(?:\\d{1,2}|100)(?:\\.\\d+)? $");
     }
+
+    public static void unitTestForEmailTrue(String data) {
+        if(checkCompanyEmail(data))
+            System.out.println("unitTestForEmailTrue is OK");
+        else
+            System.out.println("unitTestForEmailTrue is not OK");
+    }
+
+    public static void unitTestForEmailFalse(String data) {
+        if(!checkCompanyEmail(data))
+            System.out.println("unitTestForEmailFalse is OK");
+        else
+            System.out.println("unitTestForEmailFalse is not OK");
+    }
+
+    public static void unitTestFor1450True(String number) {
+        if(checkNotLessThan1450(number))
+            System.out.println("unitTestFor1450True is OK");
+        else
+            System.out.println("unitTestFor1450True is not OK");
+    }
+
+    public static void unitTestFor1450False(String number) {
+        if(!checkNotLessThan1450(number))
+            System.out.println("unitTestFor1450False is OK");
+        else
+            System.out.println("unitTestFor1450False is not OK");
+    }
+
+    public static void unitTestFor100True(String number) {
+        if(checkFrom0To100(number))
+            System.out.println("unitTestFor100True is OK");
+        else
+            System.out.println("unitTestFor100True is not OK");
+    }
+
+    public static void unitTestFor100False(String number) {
+        if(!checkFrom0To100(number))
+            System.out.println("unitTestFor100False is OK");
+        else
+            System.out.println("unitTestFor100False is not OK");
+    }
+
 }
