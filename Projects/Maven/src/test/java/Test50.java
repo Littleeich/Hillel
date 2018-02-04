5463b083a017e8f15fc9bd125db819c9d6726b2a
Littleeich <Littleeich@gmail.com>
2018-02-04 14:02:36 +0200

    Data Provider fix

--- Projects/Maven/src/test/java/Test50.java
+++ Projects/Maven/src/test/java/Test50.java
@@ -1,69 +1,58 @@
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Test50{
-
-    @Test
-    public static void unitTestForEmailTrue() {
-        Assert.assertTrue(Regex.checkCompanyEmail("sfs44sf@gmail.com "));
+    
+    @DataProvider(name = "emails")
+    public Object[][] createData1() {
+        return new Object[][] {
+                { "sfs44sf@gmail.com", Boolean.TRUE},
+                { "sfs44sf@yandex.ru", Boolean.TRUE},
+                { "sfs44sf@gmail.com, vasya@yandex.ru", Boolean.TRUE},
+                { "sfs44sf@mail.com", Boolean.FALSE},
+                { "", Boolean.FALSE},
+                { "vasya@yandex.ru, effwf@dee.ee", Boolean.FALSE},
+        };
     }
-    @Test
-    public static void unitTestForEmailFalse() { Assert.assertFalse(Regex.checkCompanyEmail("sfs44sf@mail.com "));
-    }
-    @Test
-    public static void unitTestForEmailSeveralTrue() { Assert.assertTrue(Regex.checkCompanyEmail("aadad@gmail.com, vova@yandex.ru"));
-    }
-    @Test
-    public static void unitTestForSeveralFalseEmails() { Assert.assertFalse(Regex.checkCompanyEmail("sanya@gmail.com, kolya@wwww.ww"));
-    }
-    @Test
-    public static void unitTestForEmailEmpty() { Assert.assertFalse(Regex.checkCompanyEmail(""));
-    }
-    @Test
-    public static void unitTestForNumberInsteadEmail() {Assert.assertFalse(Regex.checkCompanyEmail("1125874963"));}
-
-    @Test
-    public static void unitTestFor1450True() {
-        Assert.assertTrue(Regex.checkNotLessThan1450("10000"));
-    }
-    @Test
-    public static void unitTestFor1450withPointTrue() {
-        Assert.assertTrue(Regex.checkNotLessThan1450("4568.88"));
-    }
-    @Test
-    public static void unitTestFor1450False() {
-        Assert.assertFalse(Regex.checkNotLessThan1450("190"));
-    }
-    @Test
-    public static void unitTestFor1450withPointFalse() {
-        Assert.assertFalse(Regex.checkNotLessThan1450("190.7"));
-    }
-    @Test
-    public static void unitTestFor1450withZero(){
-        Assert.assertFalse(Regex.checkNotLessThan1450("0"));
-    }
-    @Test
-    public static void unitTestFor1450withLetters(){
-        Assert.assertFalse(Regex.checkNotLessThan1450("qwerty"));
+    @Test (dataProvider = "emails")
+    public void checkEmails(String emails, Boolean result){
+        Assert.assertEquals((Boolean)Regex.checkCompanyEmail(emails), result);
     }
 
-    @Test
-    public static void unitTestFor100True() {
-        Assert.assertTrue(Regex.checkFrom0To100("15.55"));
+    @DataProvider(name = "numbersUnder100")
+    public Object[][] createDataSmallNumbers() {
+        return new Object[][] {
+                { "100", Boolean.TRUE},
+                { "0", Boolean.TRUE},
+                { "60.85", Boolean.TRUE},
+                { "60,85", Boolean.FALSE},
+                { "760.85", Boolean.FALSE},
+                { "-60.85", Boolean.FALSE}
+
+        };
     }
-    @Test
-    public static void unitTestFor100withNegativeNumber() {Assert.assertFalse(Regex.checkFrom0To100("-965.65"));}
-    @Test
-    public static void unitTestFor100withZero() {Assert.assertTrue(Regex.checkFrom0To100("0"));}
-    @Test
-    public static void unitTestFor100False() {
-        Assert.assertFalse(Regex.checkFrom0To100("10000"));
+    @Test (dataProvider = "numbersUnder100")
+    public void checkSmallNumbers(String numbers, Boolean result){
+        Assert.assertEquals((Boolean)Regex.checkFrom0To100(numbers), result);
     }
-    @Test
-    public static void unitTestFor100Empty() {Assert.assertFalse(Regex.checkFrom0To100(""));}
-    @Test
-    public static void unitTestFor100var2() {Assert.assertTrue(Regex.checkFrom0To100("29"));}
-    @Test
-    public static void unitTestFor100Letters() {Assert.assertFalse(Regex.checkFrom0To100("qwerty"));}
+
+    @DataProvider(name = "numbers1450")
+    public Object[][] createDataBigNumbers() {
+        return new Object[][] {
+                { "1450", Boolean.TRUE},
+                { "966555", Boolean.TRUE},
+                { "1500.77", Boolean.TRUE},
+                { "60,85", Boolean.FALSE},
+                { "760.85", Boolean.FALSE},
+                { "-60.85", Boolean.FALSE},
+                { "", Boolean.FALSE},
+
+        };
+    }
+    @Test (dataProvider = "numbers1450")
+    public void checkBigNumbers(String numbers, Boolean result){
+        Assert.assertEquals((Boolean)Regex.checkNotLessThan1450(numbers), result);
+    }
 
 }
