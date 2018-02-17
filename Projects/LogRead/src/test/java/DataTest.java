f128844097274a50500dce8cf2582fb4d59f409e
Littleeich <Littleeich@gmail.com>
2018-02-17 22:21:38 +0200

    ITestResult checks

--- Projects/LogRead/src/test/java/DataTest.java
+++ Projects/LogRead/src/test/java/DataTest.java
@@ -1,4 +1,6 @@
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -15,8 +17,23 @@ public class DataTest {
         };
     }
 
-    @Test (dataProvider = "logs")
+    @Test (dataProvider = "logs", description = "checkLog")
     public void checkLogCleaner(String before, String after){
         Assert.assertEquals(Reader.LogCleaner(before), after);
+    }
+
+    @AfterMethod
+    void afterM (ITestResult testResult){
+        System.out.println("Is Succes: " + testResult.isSuccess());
+        System.out.println("Host: " + testResult.getHost());
+        System.out.println("InstanceName: " + testResult.getInstanceName());
+        System.out.println("Name: " + testResult.getName());
+        System.out.println("TestName: " + testResult.getTestName());
+        System.out.println("Description is: " + testResult.getMethod().getDescription());
+        System.out.println("Method is: " + testResult.getMethod().getMethodName());
+        System.out.println("ID is: " + testResult.getMethod().getId());
+        System.out.println("End time is: " + testResult.getEndMillis());
+        System.out.println("Start time is: " + testResult.getStartMillis());
+        System.out.println("\n\n");
     }
 }
